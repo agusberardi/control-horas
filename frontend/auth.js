32a2c5d644db40cfd0fe1ef467562169c149e028
Agustin <aguberardi@hotmail.com>
2026-02-22 20:18:35 -0300

    Fix user_id

--- frontend/auth.js
+++ frontend/auth.js
@@ -1,25 +1,23 @@
-const supabase = window.supabase.createClient(
-  "https://TU_PROYECTO.supabase.co",
-  "TU_PUBLIC_ANON_KEY"
-);
+const supabaseUrl = "Thttps://kslcypddazdiqnvnubrx.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzbGN5cGRkYXpkaXFudm51YnJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyNzM3OTEsImV4cCI6MjA4Njg0OTc5MX0.gjtV9KLwtCps_HwN53vUYmbd4ipwVB7WMgmFhp2Fy4I";
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
-let USER_ID = null;
-
+// INICIALIZAR
 async function initUser() {
-  const { data: { user } } = await supabase.auth.getUser();
+  const { data: { session } } = await supabase.auth.getSession();
 
-  if (!user) {
+  if (session) {
+    // Usuario logueado
+    document.getElementById("auth-container").style.display = "none";
+    document.getElementById("app").style.display = "block";
+  } else {
+    // No logueado
     document.getElementById("auth-container").style.display = "block";
     document.getElementById("app").style.display = "none";
-    return;
   }
-
-  UUSER_ID = 1;
-
-  document.getElementById("auth-container").style.display = "none";
-  document.getElementById("app").style.display = "block";
 }
 
+// LOGIN
 async function login() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
@@ -30,12 +28,13 @@ async function login() {
   });
 
   if (error) {
-    alert(error.message);
+    alert("Error: " + error.message);
   } else {
     location.reload();
   }
 }
 
+// REGISTER
 async function register() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
@@ -46,12 +45,13 @@ async function register() {
   });
 
   if (error) {
-    alert(error.message);
+    alert("Error: " + error.message);
   } else {
-    alert("Cuenta creada");
+    alert("Cuenta creada. Ahora inicia sesión.");
   }
 }
 
+// LOGOUT
 async function logout() {
   await supabase.auth.signOut();
   location.reload();
